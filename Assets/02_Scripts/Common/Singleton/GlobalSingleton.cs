52aa3b6a564c9e7c01a4a863d8cb99c81d6d9b9e
shin0112 <wnslcosltimo12@gmail.com>
2026-02-25 18:27:42 +0900

    feat: 전역 싱글톤 스크립트 기본 버전 작성하기

---
+++ Assets/02_Scripts/Common/Singleton/GlobalSingleton.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public abstract class GlobalSingleton<T> : MonoBehaviour where T : Component
+{
+    private static T instance;
+    public static T Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindAnyObjectByType<T>();
+                if (instance == null)
+                {
+                    var prefab = Resources.Load<GameObject>($"Managers/{typeof(T).Name}");
+                    if (prefab == null)
+                    {
+                        Debug.LogError($"Singleton: {typeof(T).Name} 프리팹을 Managers/{typeof(T).Name} 경로에서 찾을 수 없습니다.");
+                        return null;
+                    }
+                    var instance = Instantiate(prefab);
+                    instance.name = typeof(T).Name; // 생성된 오브젝트 이름 설정
+                    DontDestroyOnLoad(instance);
+                    Debug.Log($"{instance.name} dynamically created.");
+                }
+            }
+            return instance;
+        }
+    }
+
+    protected virtual void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this as T;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
