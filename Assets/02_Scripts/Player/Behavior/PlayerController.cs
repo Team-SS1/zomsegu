db84b69074267db0d9ad7d4118f9d333352a8182
Cheol <ausqkf301@naver.com>
2026-02-26 16:36:33 +0900

    feat : update Prefab, Initial Player Controller

--- Assets/02_Scripts/Player/Behavior/PlayerController.cs
+++ Assets/02_Scripts/Player/Behavior/PlayerController.cs
@@ -1,18 +1,19 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Rendering;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public int movement;
 
-    public Rigidbody2D rb;
-    public Collider2D co;
+    public Rigidbody2D Rb { get; private set; }
+    public Collider2D Co { get; private set; }
+
     private void Awake()
     {
-        rb = GetComponent<Rigidbody2D>();
-        co = GetComponent<Collider2D>();
+        Rb = GetComponent<Rigidbody2D>();
+        Co = GetComponent<Collider2D>();
     }
     private void FixedUpdate()
     {
@@ -21,6 +22,8 @@ public class PlayerController : MonoBehaviour
 
     private void Move()
     {
-
+        //Vector2 dir; // Input의 Vector2 값을 받아 사용
+        //float speed; // 이동속도의 계산값을 받아 사용
+        //Rb.velocity = dir * speed;
     }
 }
