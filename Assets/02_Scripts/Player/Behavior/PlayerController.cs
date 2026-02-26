10ce0a04132e4dc74d37f92a4d8a12d5e66bdb75
Cheol <ausqkf301@naver.com>
2026-02-26 16:28:57 +0900

    feat : Initial Player

---
+++ Assets/02_Scripts/Player/Behavior/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public int movement;
+
+    public Rigidbody2D rb;
+    public Collider2D co;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        co = GetComponent<Collider2D>();
+    }
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+
+    }
+}
