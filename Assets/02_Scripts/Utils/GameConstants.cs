ad733abfc3d15e88e6c376f4f392f35cdb269884
shin0112 <wnslcosltimo12@gmail.com>
2026-02-26 14:22:41 +0900

    feat: utils 함수 추가

--- Assets/02_Scripts/Utils/GameConstants.cs
+++ Assets/02_Scripts/Utils/GameConstants.cs
@@ -1,8 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-// 전역적으로 사용하는 상수값 정의하기 ex) String(Dialogue), Int(Item Id) 등
+﻿// 전역적으로 사용하는 상수값 정의하기 ex) String(Dialogue), Int(Item Id) 등
 public static class GameConstants
-{ 
+{
+    // 랜덤
+    public static System.Random Random = new();
 }
