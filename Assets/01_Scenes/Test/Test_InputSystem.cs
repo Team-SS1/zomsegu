a521a94681e4bdc1e1b2d7e2f3e5e96d885155f4
shin0112 <wnslcosltimo12@gmail.com>
2026-02-27 14:51:29 +0900

    test: InputSystem 예시 코드 추가

--- Assets/01_Scenes/Test/Test_InputSystem.cs
+++ Assets/01_Scenes/Test/Test_InputSystem.cs
@@ -1,20 +1,30 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Test_InputSystem : MonoBehaviour
 {
+    InputManager mg;
     Vector2 inputVec;
 
     void Start()
     {
-        InputManager mg = InputManager.Instance;
-        mg.SetLayer(InputEnum.ActionMaps.Gameplay);
-        mg.BindInput(InputEnum.ActionMaps.Gameplay, InputEnum.Actions.Move, OnMove);
+        mg = InputManager.Instance;
+
+        Example_BindInput();
     }
 
     private void Update()
     {
         transform.position += 5f * Time.deltaTime * (Vector3)inputVec;
+    }
+
+    #region Example - Bind Input
+    private void Example_BindInput()
+    {
+        mg.BindInput(InputEnum.ActionMaps.Gameplay, InputEnum.Actions.Move, OnMove);
+        mg.BindInput(InputEnum.ActionMaps.Dialogue, InputEnum.Actions.Next, OnNext);
+        Logger.Log("바인드");
     }
 
     private void OnMove(InputAction.CallbackContext context)
@@ -28,4 +38,48 @@ public class Test_InputSystem : MonoBehaviour
             inputVec = Vector2.zero;
         }
     }
+    #endregion
+
+    #region Example - Layer Control
+    [SerializeField] GameObject dialoguePanel;
+
+    public void Example_Open_Dialogue_Block_Gameplay()
+    {
+        mg.SetLayer(InputEnum.ActionMaps.Gameplay);
+
+        mg.AddLayer(InputEnum.ActionMaps.Dialogue);
+        mg.RemoveLayer(InputEnum.ActionMaps.Gameplay);
+
+        dialoguePanel.SetActive(true);
+    }
+
+    private void OnNext(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Started)
+        {
+            dialoguePanel.SetActive(false);
+
+            mg.RemoveLayer(InputEnum.ActionMaps.Dialogue);
+            mg.AddLayer(InputEnum.ActionMaps.Gameplay);
+        }
+    }
+    #endregion
+
+    #region Utils
+    public void ToggleLayer(string text)
+    {
+        Enum.TryParse(text, out InputEnum.ActionMaps actionMaps);
+
+        if (mg.HasLayer(actionMaps))
+        {
+            mg.RemoveLayer(actionMaps);
+            Logger.Log($"{actionMaps} 꺼짐");
+        }
+        else
+        {
+            mg.AddLayer(actionMaps);
+            Logger.Log($"{actionMaps} 켜짐");
+        }
+    }
+    #endregion
 }
