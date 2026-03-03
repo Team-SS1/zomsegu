22ab1b4be8ba13f66d9efa8ff116af5dc70ca7f5
Cheol <ausqkf301@naver.com>
2026-03-03 12:44:43 +0900

    feat : add Json parsing tool / add PlayerStat Json

---
+++ Assets/Resources/DataClass/PlayerStat.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+[System.Serializable]
+public class PlayerStat
+{
+	public int PlayerID;
+	public string PlayerName;
+	public float BaseAttack;
+	public float BaseMovement;
+	public float RunSpeed;
+	public int SilentWalkSpeed;
+	public int MoveSpeedMin;
+	public float BaseMaxStamina;
+	public float BaseStamina;
+	public int MaxHunger;
+	public int StartHunger;
+	public int MaxThirst;
+	public int StartThirst;
+	public int HungerDecayAmount;
+	public int ThirstDecayAmount;
+	public float NoiseRadiusWalk;
+	public float NoiseRadiusRun;
+	public float NoiseRadiusSilentWalk;
+	public float AwarenessRadiusMax;
+	public float AwarenessRadiusLimited;
+	public float AwarenessRadiusMin;
+	public float VisionRadiusMax;
+	public float VisionRadiusLimited;
+	public float VisionRadiusMin;
+	public int VisionFOVAngleMax;
+	public int VisionFOVAngleLimited;
+	public int VisionFOVAngleMin;
+	public int UnbalanceDuration;
+	public int DefaultWeaponID;
+
+	public static Dictionary<int, PlayerStat> tableDic = new Dictionary<int, PlayerStat>();
+}
