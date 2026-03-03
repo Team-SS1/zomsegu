eb2d4d699260415cebd4b8e89ba1c0329aa187df
shin0112 <wnslcosltimo12@gmail.com>
2026-03-03 12:54:26 +0900

    feat: 미리듣기에 볼륨 적용되도록 editor 코드 수정

--- Assets/Editor/Audio/AudioDataEditor.cs
+++ Assets/Editor/Audio/AudioDataEditor.cs
@@ -1,84 +1,115 @@
-using System;
-using System.Reflection;
 using UnityEditor;
+using UnityEditorInternal;
 using UnityEngine;
 
 [CustomEditor(typeof(AudioData))]
 public class AudioDataEditor : Editor
 {
-    private MethodInfo playMethod;
-    private MethodInfo stopMethod;
+    private ReorderableList list;
 
     private GUIContent playIcon;
     private GUIContent stopIcon;
 
+    private static AudioSource previewSource;
+
     private void OnEnable()
     {
-        var audioUtilType = typeof(AudioImporter).Assembly.GetType("UnityEditor.AudioUtil");
-
-        playMethod = audioUtilType.GetMethod(
-            "PlayPreviewClip",
-            BindingFlags.Static | BindingFlags.Public,
-            null,
-            new Type[] { typeof(AudioClip), typeof(int), typeof(bool) },
-            null
-        );
-
-        stopMethod = audioUtilType.GetMethod(
-            "StopAllPreviewClips",
-            BindingFlags.Static | BindingFlags.Public
-        );
-
         playIcon = EditorGUIUtility.IconContent("PlayButton");
         stopIcon = EditorGUIUtility.IconContent("PreMatQuad");
+
+        InitReorderableList();
+        EnsurePreviewSource();
     }
 
     public override void OnInspectorGUI()
     {
         serializedObject.Update();
-
-        DrawDefaultInspector();
-        EditorGUILayout.Space(10);
-
-        SerializedProperty clipsProp = serializedObject.FindProperty("audioClips");
-
-        if (clipsProp != null)
-        {
-            EditorGUILayout.LabelField("Preview", EditorStyles.boldLabel);
-
-            for (int i = 0; i < clipsProp.arraySize; i++)
-            {
-                SerializedProperty element = clipsProp.GetArrayElementAtIndex(i);
-
-                EditorGUILayout.BeginHorizontal();
-
-                EditorGUILayout.PropertyField(element, GUIContent.none);
-
-                if (GUILayout.Button(playIcon, GUILayout.Width(28), GUILayout.Height(20)))
-                {
-                    PlayClip(element.objectReferenceValue as AudioClip);
-                }
-
-                if (GUILayout.Button(stopIcon, GUILayout.Width(28), GUILayout.Height(20)))
-                {
-                    StopAll();
-                }
-
-                EditorGUILayout.EndHorizontal();
-            }
-        }
-
+        list.DoLayoutList();
         serializedObject.ApplyModifiedProperties();
     }
 
-    private void PlayClip(AudioClip clip)
+    private void EnsurePreviewSource()
+    {
+        if (previewSource != null) return;
+
+        GameObject go = EditorUtility.CreateGameObjectWithHideFlags(
+            "AudioPreview",
+            HideFlags.HideAndDontSave,
+            typeof(AudioSource)
+        );
+
+        previewSource = go.GetComponent<AudioSource>();
+    }
+
+    private void InitReorderableList()
+    {
+        SerializedProperty listProp = serializedObject.FindProperty("audioEntries");
+
+        list = new ReorderableList(serializedObject, listProp, true, true, true, true);
+
+        list.drawHeaderCallback = rect =>
+        {
+            EditorGUI.LabelField(rect, "Audio Entries");
+        };
+
+        // 🔹 높이 자동 계산
+        list.elementHeightCallback = index =>
+        {
+            var element = listProp.GetArrayElementAtIndex(index);
+            var volumeProp = element.FindPropertyRelative("volume");
+
+            float volumeHeight = EditorGUI.GetPropertyHeight(volumeProp);
+            return Mathf.Max(EditorGUIUtility.singleLineHeight, volumeHeight) + 6;
+        };
+
+        list.drawElementCallback = (rect, index, isActive, isFocused) =>
+        {
+            var element = listProp.GetArrayElementAtIndex(index);
+            var clipProp = element.FindPropertyRelative("audioClip");
+            var volumeProp = element.FindPropertyRelative("volume");
+
+            rect.y += 3;
+
+            float spacing = 4f;
+            float buttonWidth = 28f;
+
+            float totalButtonWidth = buttonWidth * 2 + spacing * 2;
+            float remainingWidth = rect.width - totalButtonWidth - spacing;
+
+            float clipWidth = remainingWidth * 0.6f;
+            float volumeWidth = remainingWidth * 0.4f;
+
+            float volumeHeight = EditorGUI.GetPropertyHeight(volumeProp);
+
+            Rect clipRect = new Rect(rect.x, rect.y, clipWidth, EditorGUIUtility.singleLineHeight);
+            Rect volumeRect = new Rect(clipRect.xMax + spacing, rect.y, volumeWidth, volumeHeight);
+            Rect playRect = new Rect(volumeRect.xMax + spacing, rect.y, buttonWidth, EditorGUIUtility.singleLineHeight);
+            Rect stopRect = new Rect(playRect.xMax + spacing, rect.y, buttonWidth, EditorGUIUtility.singleLineHeight);
+
+            EditorGUI.PropertyField(clipRect, clipProp, GUIContent.none);
+            EditorGUI.PropertyField(volumeRect, volumeProp, GUIContent.none);
+
+            if (GUI.Button(playRect, playIcon))
+                PlayClip(clipProp.objectReferenceValue as AudioClip, volumeProp.floatValue);
+
+            if (GUI.Button(stopRect, stopIcon))
+                StopAll();
+        };
+    }
+
+    private void PlayClip(AudioClip clip, float volume)
     {
         if (clip == null) return;
-        playMethod?.Invoke(null, new object[] { clip, 0, false });
+
+        previewSource.Stop();
+        previewSource.clip = clip;
+        previewSource.volume = volume;
+        previewSource.Play();
+
     }
 
     private void StopAll()
     {
-        stopMethod?.Invoke(null, null);
+        previewSource.Stop();
     }
 }
