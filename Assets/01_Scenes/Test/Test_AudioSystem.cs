24d192638afce2f5eff50fd52b1185ad00809397
shin0112 <wnslcosltimo12@gmail.com>
2026-03-04 14:15:40 +0900

    test: AudioSystem 테스트 코드 변경
    - 믹서 볼륨 정보 저장되는지 확인

--- Assets/01_Scenes/Test/Test_AudioSystem.cs
+++ Assets/01_Scenes/Test/Test_AudioSystem.cs
@@ -3,6 +3,8 @@ using UnityEngine.UI;
 
 public class Test_AudioSystem : MonoBehaviour
 {
+    [SerializeField] bool deleteMove = true;
+
     [SerializeField] Button sfxButton;
     [SerializeField] Button bgmButton;
     [SerializeField] Button stopButton;
@@ -13,9 +15,20 @@ public class Test_AudioSystem : MonoBehaviour
 
     bool toggleBgm = false;
 
+    AudioManager mg;
+
+    private void OnEnable()
+    {
+        if (mg == null) return;
+
+        masterSlider.SetValueWithoutNotify(mg.GetVolume(AudioEnum.AudioMixerGroupType.Master));
+        bgmSlider.SetValueWithoutNotify(mg.GetVolume(AudioEnum.AudioMixerGroupType.Bgm));
+        sfxSlider.SetValueWithoutNotify(mg.GetVolume(AudioEnum.AudioMixerGroupType.Sfx));
+    }
+
     private void Start()
     {
-        AudioManager mg = AudioManager.Instance;
+        mg = AudioManager.Instance;
 
         sfxButton.onClick.AddListener(() => mg.PlaySfx2D(AudioEnum.AudioName.Test_Sfx));
         bgmButton.onClick.AddListener(() =>
@@ -32,8 +45,23 @@ public class Test_AudioSystem : MonoBehaviour
         });
         stopButton.onClick.AddListener(() => mg.StopAll());
 
-        masterSlider.onValueChanged.AddListener(mg.SetMasterVolume);
-        bgmSlider.onValueChanged.AddListener(mg.SetBgmVolume);
-        sfxSlider.onValueChanged.AddListener(mg.SetSfxVolume);
+        masterSlider.value = mg.GetVolume(AudioEnum.AudioMixerGroupType.Master);
+        bgmSlider.value = mg.GetVolume(AudioEnum.AudioMixerGroupType.Bgm);
+        sfxSlider.value = mg.GetVolume(AudioEnum.AudioMixerGroupType.Sfx);
+
+        masterSlider.onValueChanged.AddListener((float value)
+            => mg.SetVolume(AudioEnum.AudioMixerGroupType.Master, value));
+        bgmSlider.onValueChanged.AddListener((float value)
+            => mg.SetVolume(AudioEnum.AudioMixerGroupType.Bgm, value));
+        sfxSlider.onValueChanged.AddListener((float value)
+            => mg.SetVolume(AudioEnum.AudioMixerGroupType.Sfx, value));
+    }
+
+    private void OnDestroy()
+    {
+        if (deleteMove)
+        {
+            PlayerPrefs.DeleteAll();
+        }
     }
 }
