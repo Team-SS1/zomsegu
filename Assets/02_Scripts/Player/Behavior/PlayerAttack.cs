0e15c256096ce3e3b3e99a737f0063fa68f00f79
Cheol <ausqkf301@naver.com>
2026-03-04 22:06:49 +0900

    feat: add PlayerAttack script

---
+++ Assets/02_Scripts/Player/Behavior/PlayerAttack.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    private Player player;
+    public bool IsAttacking {  get; private set; }
+
+    private void Awake()
+    {
+        player = GetComponent<Player>();
+    }
+    void Start()
+    {
+        
+    }
+}
