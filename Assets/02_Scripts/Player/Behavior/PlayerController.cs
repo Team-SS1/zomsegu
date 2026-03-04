ca8de7855a40a9e1b2bed148d9ad62e5740f68ec
Cheol <ausqkf301@naver.com>
2026-03-04 20:41:53 +0900

    feat: add Player Move

--- Assets/02_Scripts/Player/Behavior/PlayerController.cs
+++ Assets/02_Scripts/Player/Behavior/PlayerController.cs
@@ -3,10 +3,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.Rendering;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-
+    public Vector2 InputVec { get; private set; }
     public Rigidbody2D Rb { get; private set; }
     public Collider2D Co { get; private set; }
 
@@ -14,6 +15,14 @@ public class PlayerController : MonoBehaviour
     {
         Rb = GetComponent<Rigidbody2D>();
         Co = GetComponent<Collider2D>();
+        
+    }
+    private void Start()
+    {
+        /* --- Test Set --- */
+        InputManager.Instance.SetMaps(InputEnum.ActionMaps.Gameplay);
+        /* --- -------- --- */
+        InputManager.Instance.BindInput(InputEnum.ActionMaps.Gameplay, InputEnum.Actions.Move, OnMove);
     }
     private void FixedUpdate()
     {
@@ -22,8 +31,19 @@ public class PlayerController : MonoBehaviour
 
     private void Move()
     {
-        //Vector2 dir; // Input의 Vector2 값을 받아 사용
-        //float speed; // 이동속도의 계산값을 받아 사용
-        //Rb.velocity = dir * speed;
+        float speed = 5; // float speed; 이동속도의 계산값을 받아 사용
+        Vector2 dir = InputVec; // Vector2 dir; // Input의 Vector2 값을 받아 사용
+        Rb.velocity = dir * speed; // Rb.velocity = dir * speed;
+    }
+    private void OnMove(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            InputVec = context.ReadValue<Vector2>();
+        }
+        else if (context.canceled)
+        {
+            InputVec = Vector2.zero;
+        }
     }
 }
