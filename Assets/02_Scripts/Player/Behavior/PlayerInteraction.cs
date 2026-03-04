d471ed87e6155a09963060f1f0e6ca8bb082afa9
Cheol <ausqkf301@naver.com>
2026-03-04 20:47:52 +0900

    feat: add player interaction system
    - 플레이어 상호작용을 처리하는 PlayerInteraction 컴포넌트 구현
    - Physics2D.OverlapCircleAll과 LayerMask를 사용해 상호작용 가능한 오브젝트 탐색
    - IInteractable 인터페이스를 통해 상호작용 실행
    - 연속 상호작용 방지를 위한 interaction delay 로직 추가

---
+++ Assets/02_Scripts/Player/Behavior/PlayerInteraction.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInteraction : MonoBehaviour
+{    
+    public LayerMask interactableLayer;
+    public float interactionDelay = 0.3f;
+
+    public bool InteractionTrigger { get; private set; }
+    public bool IsInteractable { get; private set; }
+    public float InteractionRange { get; private set; } = 3;
+
+
+    void Start()
+    {
+        InputManager.Instance.BindInput(InputEnum.ActionMaps.Gameplay, InputEnum.Actions.Interact, OnInteraction);
+    }
+    public void Interaction()
+    {
+        if (IsInteractable)
+        {
+#if UNITY_EDITOR
+            Debug.Log("Interact return");
+#endif
+            return;
+        }
+
+        IsInteractable = true;
+
+        Collider2D[] hits = Physics2D.OverlapCircleAll(
+            transform.position,
+            InteractionRange,
+            interactableLayer
+        );
+
+        if(hits == null || hits.Length == 0)
+        {
+#if UNITY_EDITOR
+            Debug.Log("Interact null (no collider in range / layer)");
+#endif
+            IsInteractable = false;
+            return;
+        }
+
+        for (int i = 0; i < hits.Length; i++)
+        {
+            var hit = hits[i];
+            if (hit.TryGetComponent<IInteractable>(out var interactable))
+            {
+                interactable.OnInteract();
+            }
+        }
+#if UNITY_EDITOR
+        Debug.Log("Interact Complete");
+#endif
+        StartCoroutine(InteractionDelay(interactionDelay));
+    }
+    private IEnumerator InteractionDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        IsInteractable = false;
+    }
+    public void OnInteraction(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            InteractionTrigger = true;
+        }
+        else if (context.performed)
+        {
+            Interaction();
+        }
+        else if (context.canceled)
+        {
+            InteractionTrigger = false;
+        }
+    }
+}
