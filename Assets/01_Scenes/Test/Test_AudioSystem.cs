3323d41f7ddd73677e5fad7ee6a2684a9b5ae7bf
shin0112 <wnslcosltimo12@gmail.com>
2026-03-04 18:01:45 +0900

    test: 오디오 매니저 예시 코드 작성

--- Assets/01_Scenes/Test/Test_AudioSystem.cs
+++ Assets/01_Scenes/Test/Test_AudioSystem.cs
@@ -13,10 +13,13 @@ public class Test_AudioSystem : MonoBehaviour
     [SerializeField] Slider bgmSlider;
     [SerializeField] Slider sfxSlider;
 
+    [SerializeField] Transform followTarget;
+
     bool toggleBgm = false;
 
     AudioManager mg;
 
+    #region Unity API
     private void OnEnable()
     {
         if (mg == null) return;
@@ -49,12 +52,9 @@ public class Test_AudioSystem : MonoBehaviour
         bgmSlider.value = mg.GetVolume(AudioEnum.AudioMixerGroupType.Bgm);
         sfxSlider.value = mg.GetVolume(AudioEnum.AudioMixerGroupType.Sfx);
 
-        masterSlider.onValueChanged.AddListener((float value)
-            => mg.SetVolume(AudioEnum.AudioMixerGroupType.Master, value));
-        bgmSlider.onValueChanged.AddListener((float value)
-            => mg.SetVolume(AudioEnum.AudioMixerGroupType.Bgm, value));
-        sfxSlider.onValueChanged.AddListener((float value)
-            => mg.SetVolume(AudioEnum.AudioMixerGroupType.Sfx, value));
+        masterSlider.onValueChanged.AddListener(OnMasterValueChanged);
+        bgmSlider.onValueChanged.AddListener(OnBgmValueChanged);
+        sfxSlider.onValueChanged.AddListener(OnSfxValueChanged);
     }
 
     private void OnDestroy()
@@ -64,4 +64,51 @@ public class Test_AudioSystem : MonoBehaviour
             PlayerPrefs.DeleteAll();
         }
     }
+    #endregion
+
+    #region Example - Play Bgm/Sfx
+    private void PlayBgm()
+    {
+        mg.PlayBgm(
+            AudioEnum.AudioName.Test_Bgm,   // AudioData SO 이름
+            clipIndex: 0,                   // AudioData의 리스트 index, -1일 경우 random(기본값)
+            loop: false,                    // 루프 기본값 BGM : true / SFX : false
+            pitch: 1f                       // pitch
+            );
+    }
+
+    private void PlayTestSfx2D()
+    {
+        mg.PlaySfx2D(AudioEnum.AudioName.Test_Sfx);
+    }
+
+    private void PlayTestSfx3D_Position()
+    {
+        // 특정 위치에서 작동하는 sfx
+        mg.PlaySfx3D(AudioEnum.AudioName.Test_Sfx, Vector3.zero);
+    }
+
+    private void PlayTestSfx3D_Transform()
+    {
+        // 특정 대상을 따라가는 sfx
+        mg.PlaySfx3D(AudioEnum.AudioName.Test_Sfx, followTarget);
+    }
+    #endregion
+
+    #region Example - Audio Mixer
+    private void OnMasterValueChanged(float value)
+    {
+        mg.SetVolume(AudioEnum.AudioMixerGroupType.Master, value);
+    }
+
+    private void OnBgmValueChanged(float value)
+    {
+        mg.SetVolume(AudioEnum.AudioMixerGroupType.Bgm, value);
+    }
+
+    private void OnSfxValueChanged(float value)
+    {
+        mg.SetVolume(AudioEnum.AudioMixerGroupType.Sfx, value);
+    }
+    #endregion
 }
