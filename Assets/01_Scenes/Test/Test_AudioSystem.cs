e33215727a766955fc0f6cfe214c7feb7ee4b661
shin0112 <wnslcosltimo12@gmail.com>
2026-03-05 13:20:46 +0900

    test: 오디오 매니저 예시 및 테스트 코드 수정
    - 메서드 네이밍 변경
    - 거리 기반 sfx 테스트
    - 불필요한 SerializeField 제거

--- Assets/01_Scenes/Test/Test_AudioSystem.cs
+++ Assets/01_Scenes/Test/Test_AudioSystem.cs
@@ -1,3 +1,5 @@
+using DG.Tweening;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -5,17 +7,10 @@ public class Test_AudioSystem : MonoBehaviour
 {
     [SerializeField] bool deleteMove = true;
 
-    [SerializeField] Button sfxButton;
-    [SerializeField] Button bgmButton;
-    [SerializeField] Button stopButton;
-
+    [Header("오디오 믹서 Slider")]
     [SerializeField] Slider masterSlider;
     [SerializeField] Slider bgmSlider;
     [SerializeField] Slider sfxSlider;
-
-    [SerializeField] Transform followTarget;
-
-    bool toggleBgm = false;
 
     AudioManager mg;
 
@@ -33,28 +28,11 @@ public class Test_AudioSystem : MonoBehaviour
     {
         mg = AudioManager.Instance;
 
-        sfxButton.onClick.AddListener(() => mg.PlaySfx2D(AudioEnum.AudioName.Test_Sfx));
-        bgmButton.onClick.AddListener(() =>
-        {
-            toggleBgm = !toggleBgm;
-            if (toggleBgm)
-            {
-                mg.PlayBgm(AudioEnum.AudioName.Test_Bgm);
-            }
-            else
-            {
-                mg.StopBgm();
-            }
-        });
-        stopButton.onClick.AddListener(() => mg.StopAll());
-
         masterSlider.value = mg.GetVolume(AudioEnum.AudioMixerGroupType.Master);
         bgmSlider.value = mg.GetVolume(AudioEnum.AudioMixerGroupType.Bgm);
         sfxSlider.value = mg.GetVolume(AudioEnum.AudioMixerGroupType.Sfx);
 
-        masterSlider.onValueChanged.AddListener(OnMasterValueChanged);
-        bgmSlider.onValueChanged.AddListener(OnBgmValueChanged);
-        sfxSlider.onValueChanged.AddListener(OnSfxValueChanged);
+        AddSliderListener();
     }
 
     private void OnDestroy()
@@ -67,8 +45,9 @@ public class Test_AudioSystem : MonoBehaviour
     #endregion
 
     #region Example - Play Bgm/Sfx
-    private void PlayBgm()
+    public void Example_PlayBgm()
     {
+        Debug.Log("BGM");
         mg.PlayBgm(
             AudioEnum.AudioName.Test_Bgm,   // AudioData SO 이름
             clipIndex: 0,                   // AudioData의 리스트 index, -1일 경우 random(기본값)
@@ -77,38 +56,85 @@ public class Test_AudioSystem : MonoBehaviour
             );
     }
 
-    private void PlayTestSfx2D()
+    public void Example_PlayTestSfx2D()
     {
+        Debug.Log("SFX");
         mg.PlaySfx2D(AudioEnum.AudioName.Test_Sfx);
     }
 
-    private void PlayTestSfx3D_Position()
+    public void Example_PlayTestSfx3D_Position()
     {
         // 특정 위치에서 작동하는 sfx
         mg.PlaySfx3D(AudioEnum.AudioName.Test_Sfx, Vector3.zero);
     }
 
-    private void PlayTestSfx3D_Transform()
+    public void Example_PlayTestSfx3D_Transform()
     {
         // 특정 대상을 따라가는 sfx
         mg.PlaySfx3D(AudioEnum.AudioName.Test_Sfx, followTarget);
     }
+
+    public void Example_StopBgm()
+    {
+        mg.StopBgm();
+    }
+
+    public void Example_StopAll()
+    {
+        mg.StopAll();
+    }
     #endregion
 
     #region Example - Audio Mixer
-    private void OnMasterValueChanged(float value)
+    private void AddSliderListener()
+    {
+        masterSlider.onValueChanged.AddListener(Example_OnMasterValueChanged);
+        bgmSlider.onValueChanged.AddListener(Example_OnBgmValueChanged);
+        sfxSlider.onValueChanged.AddListener(Example_OnSfxValueChanged);
+    }
+
+    private void Example_OnMasterValueChanged(float value)
     {
         mg.SetVolume(AudioEnum.AudioMixerGroupType.Master, value);
     }
 
-    private void OnBgmValueChanged(float value)
+    private void Example_OnBgmValueChanged(float value)
     {
         mg.SetVolume(AudioEnum.AudioMixerGroupType.Bgm, value);
     }
 
-    private void OnSfxValueChanged(float value)
+    private void Example_OnSfxValueChanged(float value)
     {
         mg.SetVolume(AudioEnum.AudioMixerGroupType.Sfx, value);
+    }
+    #endregion
+
+    #region Example - Follow Movable 3D SFX
+    [Header("설정 - 이동 가능 타겟 SFX")]
+    [SerializeField] Transform followTarget;
+    [SerializeField] float startPos = -10f;
+    [SerializeField] float duration = 3f;
+    [SerializeField] float repeatingTime = 1f;
+
+    Tween tween;
+    Coroutine coroutine;
+
+    public void Example_Movable3DSFX()
+    {
+        tween?.Kill();
+        followTarget.position = (Vector3)new Vector2(startPos, 0);
+        if (coroutine != null) StopCoroutine(coroutine);
+        coroutine = StartCoroutine(Repeat3DSfx());
+        tween = followTarget.DOMoveX(-startPos, duration).OnComplete(() => StopCoroutine(coroutine));
+    }
+
+    private IEnumerator Repeat3DSfx()
+    {
+        while (true)
+        {
+            Example_PlayTestSfx3D_Transform();
+            yield return new WaitForSeconds(repeatingTime);
+        }
     }
     #endregion
 }
