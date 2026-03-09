af5630ceecb02c3d3729991df2972c8f3a3e3aad
Cheol <ausqkf301@naver.com>
2026-03-09 15:20:50 +0900

    feat: add EventEnum PlayerCondition

--- Assets/02_Scripts/Common/Enums/EventEnum.cs
+++ Assets/02_Scripts/Common/Enums/EventEnum.cs
@@ -2,6 +2,7 @@
 {
     public enum EventKey
     {
+        /* ----- Item, Inventory, Stat ----- */
         InventoryChanged,
         EquipmentChanged,
         QuickSlotChanged,
@@ -9,6 +10,14 @@
         StatChanged,
         ActiveCharacterChanged, // 플레이 캐릭터 변경
         InspectCharacterChanged, // 그냥 캐릭터 정보창 변경
+
+        /* ----- Player Condition ----- */
+        OnHungerChanged,
+        OnThirstChanged,
+        OnShockChanged,
+        OnInjuryChanged,
+        OnStaminaChanged,
+        OnTiredChanged,
 
     }
 }
