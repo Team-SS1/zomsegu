b301634f3a6b0547117d235bedff776d75da1c36
shin0112 <wnslcosltimo12@gmail.com>
2026-03-12 14:55:30 +0900

    feat: 버튼에 이벤트 연결, auto mode 설정하기

--- Assets/02_Scripts/UI/Dialogue/UIDialogue.cs
+++ Assets/02_Scripts/UI/Dialogue/UIDialogue.cs
@@ -19,6 +19,55 @@ public class UIDialogue : MonoBehaviour
     [Header("Extra")]
     [SerializeField] GameObject autoPlaying;
 
+    private bool autoMode = false;
+
+    #region Unity API
+    private void Awake()
+    {
+        skipBtn.onClick.AddListener(OnClickSkipBtn);
+        autoBtn.onClick.AddListener(OnClickAutoBtn);
+        backlogBtn.onClick.AddListener(OnClickBacklogBtn);
+        optionBtn.onClick.AddListener(OnClickOptionBtn);
+        dialogueWindowBtn.onClick.AddListener(OnClickDialogueWindowBtn);
+    }
+    #endregion
+
+    #region 버튼 이벤트
+    private void OnClickSkipBtn()
+    {
+
+    }
+
+    private void OnClickAutoBtn()
+    {
+        SetAutoMode(!autoMode);
+    }
+
+    private void OnClickBacklogBtn()
+    {
+
+    }
+
+    private void OnClickOptionBtn()
+    {
+
+    }
+
+    private void OnClickDialogueWindowBtn()
+    {
+        if (autoMode)
+        {
+            SetAutoMode(false);
+        }
+    }
+    #endregion
+
+    private void SetAutoMode(bool value)
+    {
+        autoMode = value;
+        autoPlaying.SetActive(autoMode);
+    }
+
     #region 유니티 전용
 #if UNITY_EDITOR
     private void Reset()
