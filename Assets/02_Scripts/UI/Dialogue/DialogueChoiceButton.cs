080fc3cca02a0d3b039c2caf54826cc65de18773
shin0112 <wnslcosltimo12@gmail.com>
2026-03-16 12:33:41 +0900

    feat: 선택지 버튼 스크립트, 프리팹 만들기

--- Assets/02_Scripts/UI/Dialogue/DialogueChoiceButton.cs
+++ Assets/02_Scripts/UI/Dialogue/DialogueChoiceButton.cs
@@ -4,20 +4,37 @@ using UnityEngine.UI;
 
 public class DialogueChoiceButton : MonoBehaviour
 {
-    [SerializeField] private Button button;
     [SerializeField] private TMP_Text text;
+    [SerializeField] private float horizontalPadding = 16f;
 
-    private int id;
+    private Button button;
+    private RectTransform rect;
+    private DialogueChoiceData data;
 
-    public void Init(int id, string text)
+    private void Awake()
     {
-        this.id = id;
-        this.text.text = text;
+        button = GetComponent<Button>();
+        rect = GetComponent<RectTransform>();
+    }
+
+    public void Init(DialogueChoiceData data, int no)
+    {
+        this.data = data;
+        text.text = $"{no}. {data.text}";
 
         // text 길이에 맞게 width 조정하기
-
+        float width = text.preferredWidth + horizontalPadding;
+        rect.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, width);
     }
 
     // hover 또는 select 시 폰트 수정
 
+    #region 에디터 전용
+#if UNITY_EDITOR
+    private void Reset()
+    {
+        text = GetComponentInChildren<TMP_Text>();
+    }
+#endif
+    #endregion
 }
