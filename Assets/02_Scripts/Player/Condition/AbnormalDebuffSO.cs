219f394e28b6a5788614f76bd09c03c575e8f14d
Cheol <ausqkf301@naver.com>
2026-03-16 18:19:30 +0900

    feat: add debuff value SO
    - 디버프 값을 저장하는 ScriptableObject 추가

---
+++ Assets/02_Scripts/Player/Condition/AbnormalDebuffSO.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class DebuffSet
+{
+    public float attack;
+    public float attackSpeed;
+    public float moveSpeed;
+    public float stamina;
+}
+
+[CreateAssetMenu(fileName = "AbnormalDebuffData", menuName = "SO/Abnormal/AbnormalDebuffData")]
+public class AbnormalDebuffData : ScriptableObject
+{
+    [Header("Common")]
+    public DebuffSet common;
+
+    [Header("Hunger")]
+    public DebuffSet hungry;
+    public DebuffSet starving;
+
+    [Header("Thirst")]
+    public DebuffSet thirsty;
+    public DebuffSet dehydrated;
+
+    [Header("Shock")]
+    public DebuffSet shock;
+    public DebuffSet heavyShock;
+    public DebuffSet severeShock;
+
+    [Header("Injury")]
+    public DebuffSet injured;
+
+    [Header("Tired")]
+    public DebuffSet tired;
+}
