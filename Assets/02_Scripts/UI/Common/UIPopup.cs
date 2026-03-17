028ede5deda7c36e3f5df14fae08b0a039218bd8
shin0112 <wnslcosltimo12@gmail.com>
2026-03-17 15:33:23 +0900

    feat: 팝업용 ui 만들기

---
+++ Assets/02_Scripts/UI/Common/UIPopup.cs
@@ -0,0 +1,68 @@
+﻿using InputEnum;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+
+public class UIPopup : MonoBehaviour
+{
+    [SerializeField] private TMP_Text text;
+    [SerializeField] private BaseButton btnYes;
+    [SerializeField] private BaseButton btnNo;
+
+    private UnityAction callback;
+
+    private void OnEnable()
+    {
+        btnYes.onClick.AddListener(OnClickYes);
+        btnNo.onClick.AddListener(OnClickNo);
+    }
+
+    private void Start()
+    {
+        InputManager.Instance.BindInput(ActionMaps.UI, Actions.Close, OnClose);
+    }
+
+    private void OnDisable()
+    {
+        btnYes.onClick.RemoveListener(OnClickYes);
+        btnNo.onClick.RemoveListener(OnClickNo);
+    }
+
+    public void OpenPopup(string text, UnityAction callback = null)
+    {
+        this.text.text = text;
+        this.callback = callback;
+    }
+
+    private void OnClickYes()
+    {
+        callback?.Invoke();
+        gameObject.SetActive(false);
+        btnYes.onClick.RemoveListener(OnClickYes);
+    }
+
+    private void OnClickNo()
+    {
+        gameObject.SetActive(false);
+    }
+
+    private void OnClose(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    #region 에디터 전용
+#if UNITY_EDITOR
+    private void Reset()
+    {
+        text = transform.FindChild<TMP_Text>("Text");
+        btnYes = transform.FindChild<BaseButton>("Btn_Yes");
+        btnNo = transform.FindChild<BaseButton>("Btn_No");
+    }
+#endif
+    #endregion
+}
