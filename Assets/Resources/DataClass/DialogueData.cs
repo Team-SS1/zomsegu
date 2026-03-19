55523a15a8e11cb7257c6660a1e86bf7277b4cbc
shin0112 <wnslcosltimo12@gmail.com>
2026-03-19 16:26:16 +0900

    feat: 대화 발화자가 플레이어 캐릭터인지 확인하는 필드 추가

--- Assets/Resources/DataClass/DialogueData.cs
+++ Assets/Resources/DataClass/DialogueData.cs
@@ -8,6 +8,7 @@ public class DialogueData
     public string portrait;
     public string speaker;
     public string text;
+    public bool isPlayer;
     public List<int> choiceIds;
     public int nextDialogueId = -1;
 
@@ -21,6 +22,7 @@ public class DialogueData
                 speaker = "1",
                 text ="지금 이 순간이 얼마나 작고 평범해 보이더라도 괜찮다. 대부분의 변화는 아주 사소한 결심에서 시작되고, 그 결심이 쌓여 결국 큰 흐름이 된다. 오늘 조금 더 배우고, 조금 더 버티고, 조금 더 앞으로 나아갔다면 이미 충분히 잘하고 있는 것이다. 길이 멀어 보일수록 속도를 재촉하기보다 방향을 잃지 않는 것이 더 중요하다. 결국 끝까지 걸어간 사람만이 자신이 얼마나 멀리 왔는지 알게 된다.",
                 nextDialogueId = 2,
+                isPlayer = true,
             }
         },
         { 2, new DialogueData()
