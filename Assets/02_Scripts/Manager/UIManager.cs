50190e3d9a1fea972b65eff575a68832403a4cfa
shin0112 <wnslcosltimo12@gmail.com>
2026-03-19 20:41:15 +0900

    feat: ui manager 만들기
    - 팝업, 일반 ui 분리해서 관리하도록 설정

---
+++ Assets/02_Scripts/Manager/UIManager.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : GlobalSingleton<UIManager>
+{
+    [SerializeField] private Canvas canvasPrefab;
+
+    private readonly Dictionary<Type, BaseUI> uiCache = new();  // 리소스 캐시
+
+    private RectTransform uiRoot;
+    private RectTransform popupRoot;
+
+    private readonly Dictionary<Type, BaseUI> panelMap = new(); // 기본 ui
+    private readonly List<UIPopup> popupStack = new();          // 팝업 ui
+
+    #region Unity API
+    protected override void Awake()
+    {
+        base.Awake();
+
+        uiRoot = Instantiate(canvasPrefab).GetComponent<RectTransform>();
+        popupRoot = Instantiate(canvasPrefab).GetComponent<RectTransform>();
+
+        uiRoot.name = "UI_Root";
+        popupRoot.name = "UI_Popup_Root";
+    }
+    #endregion
+
+    #region 씬 관리
+    protected override void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        base.OnSceneLoaded(scene, mode);
+
+        panelMap.Clear();
+        popupStack.Clear();
+    }
+    #endregion
+
+    #region 생성 / 조회
+    /// <summary>
+    /// ui 프리팹 가져오기
+    /// gameobject 활성화 상태를 지정하고 싶을 경우 active 사용
+    /// </summary>
+    public T GetOrCreateUI<T>(bool active = false) where T : BaseUI
+    {
+        var type = typeof(T);
+
+        if (panelMap.TryGetValue(type, out BaseUI ui))  // 이미 존재
+        {
+            ui.gameObject.SetActive(active);
+            return ui as T;
+        }
+
+        T prefab = GetResource<T>(active);
+        ui = Instantiate(prefab, uiRoot);
+
+        panelMap[type] = ui;
+
+        return ui as T;
+    }
+
+    private T CreatePopup<T>(bool active) where T : UIPopup
+    {
+        T ui = GetResource<T>(active);
+        ui.transform.SetParent(popupRoot);
+
+        if (active)
+        {
+            popupStack.Add(ui);
+        }
+
+        return ui;
+    }
+
+    private T GetResource<T>(bool active) where T : BaseUI
+    {
+        var type = typeof(T);
+
+        if (uiCache.TryGetValue(type, out BaseUI ui))
+        {
+            return ui as T;
+        }
+
+        var prefab = Resources.Load<T>($"UI/{type.Name}");
+
+        if (prefab == null)
+        {
+            Logger.LogWarning($"{type} 프리팹 못찾음. 경로: Resources/UI");
+            return null;
+        }
+
+        uiCache[type] = prefab;
+        return prefab;
+    }
+    #endregion
+
+    #region 열기 / 닫기 - ui
+    public void ShowUI<T>() where T : BaseUI
+    {
+        T ui = GetOrCreateUI<T>(true);
+    }
+
+    public void HideUI<T>() where T : BaseUI
+    {
+        if (!panelMap.TryGetValue(typeof(T), out BaseUI ui)) return;
+        ui.gameObject.SetActive(false);
+    }
+
+    public void HideAllUI()
+    {
+        foreach (BaseUI ui in panelMap.Values)
+        {
+            ui.gameObject.SetActive(false);
+        }
+    }
+    #endregion
+
+    #region 열기 / 닫기 - 팝업 ui
+    public T OpenPopup<T>() where T : UIPopup
+    {
+        T ui = CreatePopup<T>(true);
+        ui.OnUIClick += ClickPopup;
+        return ui;
+    }
+
+    public void CloseTopPopup()
+    {
+        popupStack[^1].gameObject.SetActive(false);
+        popupStack.RemoveAt(popupStack.Count - 1);
+    }
+
+    public void CloseAllPopups()
+    {
+        for (int i = popupStack.Count - 1; i >= 0; i--)
+        {
+            popupStack[i].gameObject.SetActive(false);
+            popupStack.RemoveAt(i);
+        }
+    }
+
+    private void ClickPopup(UIPopup ui)
+    {
+        int idx = popupStack.IndexOf(ui);
+        popupStack.RemoveAt(idx);
+        popupStack.Add(ui);
+    }
+    #endregion
+
+    #region Utils
+    /// <summary>
+    /// ui (팝업 제외) 활성화 확인
+    /// </summary>
+    public bool IsOpen<T>() where T : BaseUI
+    {
+        if (!panelMap.TryGetValue(typeof(T), out BaseUI ui))
+        {
+            return false;
+        }
+
+        return ui.gameObject.activeSelf;
+    }
+
+    /// <summary>
+    /// ui 정리하고 싶을 경우 (ex. 씬 전환)
+    /// </summary>
+    public void ClearCache() => uiCache.Clear();
+    #endregion
+}
