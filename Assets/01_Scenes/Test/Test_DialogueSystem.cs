6009e0ac86fd25cf7d8110771dca5bbf606b605b
shin0112 <wnslcosltimo12@gmail.com>
2026-03-23 16:00:11 +0900

    test: 팝업 ui 테스트 코드 및 오브젝트 추가

--- Assets/01_Scenes/Test/Test_DialogueSystem.cs
+++ Assets/01_Scenes/Test/Test_DialogueSystem.cs
@@ -7,6 +7,15 @@ public class Test_DialogueSystem : MonoBehaviour
         //AudioManager.Instance.PlayBgm(AudioEnum.AudioName.Test_Bgm, fadeDuration: 1f);
     }
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            UIConfirmPopup ui = UIManager.Instance.OpenPopup<UIConfirmPopup>();
+            ui.Register("open pop up ui", null);
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         var ui = UIManager.Instance.GetPanel<UIDialogue>(true);
