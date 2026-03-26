038c1ed5549c4f79213c2cb1631f236a757ae7c6
shin0112 <wnslcosltimo12@gmail.com>
2026-03-26 11:38:43 +0900

    feat: base ui에 ui enum order 설정하기

--- Assets/02_Scripts/UI/Common/BaseUI.cs
+++ Assets/02_Scripts/UI/Common/BaseUI.cs
@@ -2,6 +2,12 @@
 
 public abstract class BaseUI : MonoBehaviour
 {
+    [SerializeField] private UIEnum.UIOrder order;
+    [SerializeField] private bool dontDestroy;
+
+    public UIEnum.UIOrder Order => order;
+    public bool DontDestroy => dontDestroy;
+
     // ===== Unity API ===== 
     private void Awake()
     {
