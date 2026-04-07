4f3299c52611f773004a5ace938164a9a21af802
Cheol <ausqkf301@naver.com>
2026-04-07 16:44:49 +0900

    feat: add TestInteractionObject Script

---
+++ Assets/02_Scripts/Player/TestInteractionObject.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestInteractionObject : MonoBehaviour, IInteractable
+{
+    public void OnInteract()
+    {
+        
+    }
+}
