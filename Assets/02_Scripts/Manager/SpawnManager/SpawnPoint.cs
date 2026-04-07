320da946a3f68d0ad73843eec58be276105f9275
Cheol <ausqkf301@naver.com>
2026-04-07 15:51:28 +0900

    feat: add FixedSpawnPoint / SpawnPoint
    고정 위치 스폰을 위한 FixedSpawnPoint 추가 (플레이어, 파트너, NPC)
    랜덤 스폰을 위한 SpawnPoint 추가 (몬스터, NPC)
    고정 스폰과 랜덤 스폰 역할을 분리

---
+++ Assets/02_Scripts/Manager/SpawnManager/SpawnPoint.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPoint : MonoBehaviour
+{
+    [SerializeField] private SpawnData spawnData;
+    public SpawnData SpawnData => spawnData;
+
+    private void Awake()
+    {
+        if (spawnData == null)
+        {
+            Debug.Log("Spawn Data is Null");
+            return;
+        }
+    }
+}
