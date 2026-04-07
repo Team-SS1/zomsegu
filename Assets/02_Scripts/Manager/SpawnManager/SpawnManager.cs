1c294b96a0743c7d1bd460f1a344945066ddd79f
Cheol <ausqkf301@naver.com>
2026-04-07 15:45:57 +0900

    feat: add SpawnManager spawn logic
    SpawnPoint 데이터를 받아 스폰을 실행하는 로직 구현
    설정된 개수와 반경을 기준으로 랜덤 프리팹 반복 생성 기능 추가

---
+++ Assets/02_Scripts/Manager/SpawnManager/SpawnManager.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using SpawnEnum;
+using UnityEngine;
+
+public class SpawnManager : GlobalSingleton<SpawnManager>
+{
+    [SerializeField] private SpawnPrefabDatabase database;
+
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+
+    /// <summary>
+    /// Random ID, Position Spawn * Random Count
+    /// </summary>
+    /// <param name="point"></param>
+    public void Spawn(SpawnPoint point)
+    {
+        SpawnData data = point.SpawnData;
+
+        if (data == null)
+        {
+            Debug.LogWarning("SpawnData가 없습니다.");
+            return;
+        }
+        for (int i = 0; i < data.SpawnCount; i++)
+        {
+            int randomIndex = Random.Range(0, data.SpawnId.Length);
+            SpawnID randomID = data.SpawnId[randomIndex];
+
+            GameObject prefab = database.GetPrefab(randomID);
+
+            if (prefab == null)
+            {
+                Debug.LogWarning($"Prefab 없음: {randomID}");
+                return;
+            }
+
+            float radius = data.SpawnRadius; // SpawnData에 있어야 함
+            Vector2 offset = Random.insideUnitCircle * radius;
+
+            Vector3 spawnPos = point.transform.position + new Vector3(offset.x, offset.y, 0f);
+
+            Instantiate(prefab, spawnPos, Quaternion.identity);
+        }
+    }
+    /// <summary>
+    /// Fixed Spawn / Player, Partner, NPC
+    /// </summary>
+    /// <param name="spawnId"></param>
+    /// <param name="spawnPoint"></param>
+    /// <returns></returns>
+    public GameObject Spawn(SpawnID spawnId, Transform spawnPoint)
+    {
+        if (spawnPoint == null)
+        {
+            Debug.LogWarning("SpawnPoint가 없습니다.");
+            return null;
+        }
+
+        GameObject prefab = database.GetPrefab(spawnId);
+
+        if (prefab == null)
+        {
+            Debug.LogWarning($"Prefab 없음: {spawnId}");
+            return null;
+        }
+
+        return Instantiate(prefab, spawnPoint.position, spawnPoint.rotation);
+    }
+}
