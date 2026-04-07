79da86c71d11fb98149f34b17bc80166a5334bcf
Cheol <ausqkf301@naver.com>
2026-04-07 15:45:38 +0900

    feat: add SpawnPoint data binding
    SpawnData 참조를 저장하고 제공하는 SpawnPoint 컴포넌트 추가
    위치별 스폰 설정을 적용할 수 있는 구조 마련

---
+++ Assets/02_Scripts/Manager/SpawnManager/SpawnData.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using SpawnEnum;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SpawnData", menuName = "SO/Spawn/SpawnData")]
+public class SpawnData : ScriptableObject
+{
+    [Header("Info")]
+    [SerializeField] private SpawnType spawnType;
+    [SerializeField] private SpawnID[] spawnId;
+    [SerializeField] private int spawnCount = 1;
+    [SerializeField] private bool triggerSpawn = true;
+
+    [Header("Spawn Range")]
+    [SerializeField] private bool spawnRandom = false;
+    [SerializeField] private float spawnRadius = 3f;
+    [SerializeField] private float spawnTriggerRadius = 1.5f;
+
+    [Header("Respawn")]
+    [SerializeField] private bool respawn = false;
+    [SerializeField] private float respawnDelay = 0f;
+
+    public SpawnType SpawnType => spawnType;
+    public SpawnID[] SpawnId => spawnId;
+    public bool TriggerSpawn => triggerSpawn;
+    public int SpawnCount => spawnCount;
+    public bool SpawnRandom => spawnRandom;
+    public float SpawnRadius => spawnRadius;
+    public float SpawnTriggerRadius => spawnTriggerRadius;
+    public bool Respawn => respawn;
+    public float RespawnDelay => respawnDelay;
+}
