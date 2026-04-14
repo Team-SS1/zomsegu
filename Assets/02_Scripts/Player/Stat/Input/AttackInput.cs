24a822b510ec44292c28bb54ab18cd719590459e
Cheol <ausqkf301@naver.com>
2026-04-14 15:16:15 +0900

    feat: add Attack, AttackSpeed, Movement input scripts
    핵심 스탯 입력 스크립트 추가

---
+++ Assets/02_Scripts/Player/Stat/Input/AttackInput.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class AttackInput : MonoBehaviour
+{
+    public float baseAttack;
+    public float weaponAttack;
+    public float consumeBuff;
+    public float otherBuffAddition;
+    public float otherBuffmultiplication = 1f;
+
+    public float conditionDebuff;
+    
+    public float painDebuff;
+    public float painkillerBuff;
+
+    public float minAttack = 30f;
+}
