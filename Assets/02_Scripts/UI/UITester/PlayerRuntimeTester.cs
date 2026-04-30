fe481f9459dad8cfb34b82123488381438c89125
Minji <kuminji125@gmail.com>
2026-04-30 17:53:00 +0900

    feat: 플레이어 게이지(WIP)
    일단 테스트로 데이터 올려서 해봤는데 깜빡이는게 안 예뻐서 두트윈으로 만들어 보겠습니다

---
+++ Assets/02_Scripts/UI/UITester/PlayerRuntimeTester.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRuntimeTester : MonoBehaviour // 잠시 테스트용으로 붙인 후 삭제할 컴포넌트, 게이지 확인용
+{
+    [Header("Reference")]
+    [SerializeField] private Player player;
+    [SerializeField] private PlayerCondition playerCondition;
+
+    private void Start()
+    {
+        if(player == null || playerCondition == null || PlayerDataManager.Instance == null)
+        {
+            Debug.LogError("PlayerRuntimeTester: Missing references. Please assign Player, PlayerCondition, and ensure PlayerDataManager is initialized.");
+            return;
+        }
+        player.Init();
+        playerCondition.Init();
+        Debug.Log("PlayerRuntimeTester: Initialization complete. Player and PlayerCondition are set up.");
+    }
+}
