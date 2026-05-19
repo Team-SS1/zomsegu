69db525a42b176890b55480b757061733f51a17c
Minji <kuminji125@gmail.com>
2026-05-19 13:26:51 +0900

    feat: MainPanel UIManager 연결

---
+++ Assets/02_Scripts/UI/Panel/UIMainPanel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIMainPanel : UIPopup
+{
+    [Header("Button")]
+    [SerializeField] private Button closeButton;
+
+    protected override void AwakeInternal()
+    {
+        base.AwakeInternal();
+        if(closeButton != null )
+            closeButton.onClick.AddListener(Close);
+    }
+    protected override void DestroyInternal()
+    {
+        if(closeButton != null)
+            closeButton.onClick.RemoveListener(Close);
+        base.DestroyInternal();
+    }
+}
