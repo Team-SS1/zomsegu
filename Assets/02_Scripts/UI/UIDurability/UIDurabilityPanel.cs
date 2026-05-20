634fe5694d6903a7bbd944eed819344cd5668930
Minji <kuminji125@gmail.com>
2026-05-20 15:28:40 +0900

    feat: 내구도 탐색창 만듬

--- Assets/02_Scripts/UI/UIDurability/UIDurabilityPanel.cs
+++ Assets/02_Scripts/UI/UIDurability/UIDurabilityPanel.cs
@@ -1,8 +1,80 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
+﻿using UnityEngine;
+using PlayerEnum;
+using EventEnum;
+using UnityEngine.UI;
+using Unity.VisualScripting;
 public class UIDurabilityPanel : BaseUI
 {
-    
+    [Header("Buttons")]
+    [SerializeField] private Button openInventoryButton;
+    [SerializeField] private Button closeButton;
+
+    [Header("Slots")]
+    [SerializeField] private UIDurabilitySlot weaponSlot;
+    [SerializeField] private UIDurabilitySlot headSlot;
+    [SerializeField] private UIDurabilitySlot bodySlot;
+    [SerializeField] private UIDurabilitySlot legSlot;
+
+    protected override void AwakeInternal()
+    {
+        base.AwakeInternal();
+        if (openInventoryButton != null)
+            openInventoryButton.onClick.AddListener(OnClickOpenInventory);
+
+        if (closeButton != null)
+            closeButton.onClick.AddListener(Close);
+    }
+    protected override void DestroyInternal()
+    {
+        if (openInventoryButton != null)
+            openInventoryButton.onClick.RemoveListener(OnClickOpenInventory);
+
+        if(closeButton != null)
+            closeButton.onClick.RemoveListener(Close);
+
+        base.DestroyInternal();
+    }
+    protected override void EnableInternal()
+    {
+        base.EnableInternal();
+
+        EventManager.Subscribe<PlayerType>(EventKey.ActiveCharacterChanged, OnActiveCharacterChanged);
+        EventManager.Subscribe<PlayerType>(EventKey.EquipmentDurabilityChanged, OnEquipmentChanged);
+        EventManager.Subscribe<PlayerType>(EventKey.EquipmentChanged, OnEquipmentChanged);
+
+        Refresh(PlayerManager.Instance.CurrentActivePlayer);
+    }
+    protected override void DisableInternal()
+    {
+        EventManager.UnSubscribe<PlayerType>(EventKey.ActiveCharacterChanged, OnActiveCharacterChanged);
+        EventManager.UnSubscribe<PlayerType>(EventKey.EquipmentDurabilityChanged, OnEquipmentChanged);
+        EventManager.UnSubscribe<PlayerType>(EventKey.EquipmentChanged, OnEquipmentChanged);
+
+        base.DisableInternal();
+    }
+    private void OnActiveCharacterChanged(PlayerType playerType)
+    {
+        Refresh(playerType);
+    }
+    private void OnEquipmentChanged(PlayerType playerType)
+    {
+        if (playerType != PlayerManager.Instance.CurrentActivePlayer) return;
+
+        Refresh(playerType);
+    }
+    private void Refresh(PlayerType playerType)
+    {
+        if (weaponSlot != null) weaponSlot.Refresh(playerType);
+        if(headSlot != null) headSlot.Refresh(playerType);
+        if(bodySlot != null) bodySlot.Refresh(playerType);
+        if(legSlot != null) legSlot.Refresh(playerType);
+    }
+    private void OnClickOpenInventory()
+    {
+        //이따가 만들 예정
+    }
+    private void Close()
+    {
+        gameObject.SetActive(false);
+    }
 }
--- Assets/02_Scripts/UI/UIDurability/UIDurabilitySlot.cs
+++ Assets/02_Scripts/UI/UIDurability/UIDurabilitySlot.cs
@@ -1,8 +1,73 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using ItemEnum;
+using UnityEngine.UI;
+using TMPro;
+using PlayerEnum;
 
 public class UIDurabilitySlot : MonoBehaviour
 {
-    
+    [Header("Slot Type")]
+    [SerializeField] private EquipSlotType slotType;
+
+    [Header("UI")]
+    [SerializeField] private Image iconImg;
+    [SerializeField] private TextMeshProUGUI durabilityTXT;
+
+    public void Refresh(PlayerType playerType)
+    {
+        int itemId = EquipmentQueryService.GetEquippedItemId(playerType, slotType);
+        ItemStack instance = EquipmentQueryService.GetEquippedInstance(playerType, slotType);
+
+        if(itemId == 0)
+        {
+            Clear();
+            return;
+        }
+
+        SetIcon(itemId);
+
+        if(instance != null && instance.HasDurability)
+        {
+            durabilityTXT.text = $"{instance.durability}/{instance.maxDurability}";
+        }
+        else if(ItemDB.IsRangedWeapon(itemId))
+        {
+            PlayerData playerData = PlayerDataManager.Instance.GetPlayerData(playerType);
+            if (playerData == null) durabilityTXT.text = "";
+            Inventory inventory = playerData.Inventory;
+            if (inventory == null) durabilityTXT.text = "";
+            durabilityTXT.text = inventory.GetStackAmount(itemId)>0 ? inventory.GetStackAmount(itemId).ToString() : "";
+        }
+        else
+        {
+            durabilityTXT.text = "";
+        }
+    }
+    private void Clear()
+    {
+        if(iconImg != null)
+        {
+            iconImg.sprite = null;
+            iconImg.enabled = false;
+        }
+        if(durabilityTXT != null)
+            durabilityTXT.text = "";
+    }
+    private void SetIcon(int itemId)
+    {
+        if (iconImg == null) return;
+
+        string iconPath = ItemDB.GetIconPath(itemId);
+
+        if (string.IsNullOrEmpty(iconPath))
+        {
+            iconImg.sprite = null;
+            iconImg.enabled = false;
+            return;
+        }
+
+        Sprite icon = Resources.Load<Sprite>(iconPath);
+        iconImg.sprite = icon;
+        iconImg.enabled = icon != null;
+    }
 }
