f105abb404a278d8f3b9dec6c4a68249587cfd89
경칠이람 <taro1005@naver.com>
2026-05-27 12:28:33 +0900

    feat: WildDog Move

---
+++ Assets/02_Scripts/Monster/WildDog/WildDogMoveState.cs
@@ -0,0 +1,21 @@
+public class WildDogMoveState : IWildDogState
+{
+    private readonly WildDog dog;
+
+    public WildDogMoveState(WildDog dog)
+    {
+        this.dog = dog;
+    }
+
+    public void Enter()
+    {
+    }
+
+    public void Tick()
+    {
+    }
+
+    public void Exit()
+    {
+    }
+}
